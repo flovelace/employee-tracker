103e9c49ce114c8b11469a98dfc13e7ae26d24fc
fairfax <lovelace.af@gmail.com>
2021-11-17 00:34:26 -0500

    finished populating seeds. Tables functioning as expected

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,7 +9,7 @@ INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Engineering Manager', 125000, 1),
 ('Customer Support Manager', 95000, 1),
-('Legal Counsel', 150000, 1),
+('Senior Legal Counsel', 150000, 1),
 ('Senior Developer', 90000, 2),
 ('Junior Developer', 60000, 2),
 ('Intern Developer', 40000, 2),
@@ -21,4 +21,14 @@ INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
 ('Jill', 'Valentine', 1, NULL),
 ('Elaine', 'Benes', 2, NULL),
-('Guybrush', 'Threepwood', 3, NULL);
+('Guybrush', 'Threepwood', 3, NULL),
+('Art', 'Vandelay', 4, 1),
+('Chris', 'Lightfellow', 4, 1),
+('Geralt', 'of Rivia', 5, 1),
+('Cloud', 'Strife', 5, 1),
+('Gandalf', 'Stormcrow', 6, 1),
+('Aerith', 'Gainsborough', 7, 2),
+('Chloe', 'Price', 8, 2),
+('Max', 'Caulfield', 8, 2),
+('Lionel', 'Hutz', 9, 3),
+('Phoenix', 'Wright', 9, 3);
